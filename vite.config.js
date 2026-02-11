24d36224693e98349f8b41580c9a265a179cc8d0
Nick Steiner <nick.steiner@gmail.com>
2026-02-11 11:36:34 -0500

    Switch back to relative paths for assets

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',   // Absolute paths for JupyterHub proxy
+  base: './',   // Relative paths for JupyterHub proxy
   root: 'app',
   resolve: {
     alias: {
