e7d61b94466178b02c444c269e63793133af3f2e
Nick Steiner <nick.steiner@gmail.com>
2026-02-11 11:35:15 -0500

    Fix base path for JupyterHub proxy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',   // relative paths — required for JupyterHub proxy
+  base: '/',   // Absolute paths for JupyterHub proxy
   root: 'app',
   resolve: {
     alias: {
